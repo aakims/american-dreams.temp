f1847c5fb8b176557653334f517222509e739b69
aakims <alanakim12@gmail.com>
2018-05-02 22:56:40 -0400

    filtering foundation achieved/ data updated
    found a misexport for worksite state-lev so recreated all of the geojsons

--- js/filter.js
+++ js/filter.js
@@ -1,101 +1,113 @@
 var resultKey = {
-    'certified' : 'CERT',
-    'denied' : 'DEN',
-    'withdrawn' : 'WD'
+    'CERTIFIED': 'CERT',
+    'DENIED': 'DEN',
+    'WITHDRAWN': 'WD'
 };
 
 var subjectKey = {
-    'employer' : 'emp',
-    'worksite' : 'work'
+    'By Employer': 'employer',
+    'By Worksite': 'worksite'
 };
 
 var quarterList = [
-'2015 Q1',
-'2015 Q2',
-'2015 Q3',
-'2015 Q4',
-'2016 Q1',
-'2016 Q2',
-'2016 Q3',
-'2016 Q4',
-'2017 Q1',
-'2017 Q2',
-'2017 Q3',
-'2017 Q4',
-'2018 Q1'
+    '2015 Q1',
+    '2015 Q2',
+    '2015 Q3',
+    '2015 Q4',
+    '2016 Q1',
+    '2016 Q2',
+    '2016 Q3',
+    '2016 Q4',
+    '2017 Q1',
+    '2017 Q2',
+    '2017 Q3',
+    '2017 Q4',
+    '2018 Q1'
 ];
 
 var quarterKey = {
-'2015 Q1' : '15Q1',
-'2015 Q2' : '15Q2',
-'2015 Q3' : '15Q3',
-'2015 Q4' : '15Q4',
-'2016 Q1' : '16Q1',
-'2016 Q2' : '16Q2',
-'2016 Q3' : '16Q3',
-'2016 Q4' : '16Q4',
-'2017 Q1' : '17Q1',
-'2017 Q2' : '17Q2',
-'2017 Q3' : '17Q3',
-'2017 Q4' : '17Q4',
-'2018 Q1' : '18Q1'
+    '2015 Q1': '15Q1',
+    '2015 Q2': '15Q2',
+    '2015 Q3': '15Q3',
+    '2015 Q4': '15Q4',
+    '2016 Q1': '16Q1',
+    '2016 Q2': '16Q2',
+    '2016 Q3': '16Q3',
+    '2016 Q4': '16Q4',
+    '2017 Q1': '17Q1',
+    '2017 Q2': '17Q2',
+    '2017 Q3': '17Q3',
+    '2017 Q4': '17Q4',
+    '2018 Q1': '18Q1'
 };
 
-var thisData; 
+var thisData;
 var colList = ['APP', 'WORKER', 'AVGSAL', 'AVGPW', 'TOPNAIC5', 'TOPJOB', 'TOPCITY', 'PCTINSTATE'];
-var selectKeys; 
+var selectKeys;
+var quarter, result, subject;
 
-var actualDownload = function(urlStr) {
-        $.ajax(urlStr).then(function(res) {
-            //console.log(res); 
-            thisData = res;
-            return res;
-        });
+var wantedRes = $('#result option:selected').text();
+var wantedSubj = $('#subject option:selected').text();
+result = resultKey[wantedRes];
+subject = subjectKey[wantedSubj];
+quarter = '2016 Q1';
 
-var allFields = _.unique(_.flatten( _.map(nextData.features, function (feature) { return _.allKeys(feature.properties)})));
+var getData = function(subject) { // "employer" vs "worksite" 
 
-var defineData = function(subject, quarter, result) { // subject: employer vs worksite
+    urlCntyStr = "https://raw.githubusercontent.com/aakims/american-dreams.temp/master/data//geojson/byCounty_" + subject + ".geojson",
+    urlStateStr = "https://raw.githubusercontent.com/aakims/american-dreams.temp/master/data/geojson/byState_" + subject + ".geojson";
+
+};
+
+getData(subject); 
+
+var actualDownload = 
+    $.ajax(urlStateStr).then(function(res) {
+        //console.log(res); 
+        //thisData = res;
+        return res;
+    });
+
+var allFields = function(fedData) {
+    // console.log (_.unique(_.flatten(_.map(fedData.features, function(feature) {
+    //     return _.allKeys(feature.properties)
+    // }))));
+    return (_.unique(_.flatten(_.map(fedData.features, function(feature) {
+        return _.allKeys(feature.properties)
+    })))
+)};
+
+var defineData = function(fedData) { // subject: employer vs worksite
 
     var sprefx = subjectKey[subject],
-    qprefx = quarterKey[quarter],
-    rprefx = resultKey[result],
-    uprefx = sprefx + qprefix + rprefix; 
-    console.log("uprefx": uprefx);
+        //qprefx = quarterKey[quarter],
+        rprefx = resultKey[result];
+        //uprefx = sprefx + qprefx + rprefx;
+    //console.log("uprefx:", uprefx);
+    console.log(rprefx);
 
-    selectKeys = _.chain(allFields)
-        //.filter(function(field) { return field.split("_")[1] === qprefix }) // quarter
-        .filter(function(field) { return field.split("_")[2] === rprefix }) // result 
+    selectKeys = _.chain(allFields(fedData))
+        .filter(function(field) { return field.split("_")[2] === rprefx }) // result 
         .value();
 
     console.log("selectKeys: ", selectKeys);
 
     // prefix format: emp_15Q2_CERT_WORKER
 
-    thisData = _.chain(fedSet.features)
+    thisData = _.chain(fedData.features)
         .map(function(feature) {
             feature.properties = _.pick(feature.properties, selectKeys);
             //console.log(feature.properties);
             //console.log(feature);
             return feature;
         })
-        .map(function(feature) {
-            feature.properties["gtime"] = new Date(feature.properties["unixt"] * 1000 + 18000000);
-            return feature;
-        })
         .value();
 
-    thisDataF = thisData[0],
-    thisDataL = thisData[thisData.length - 1];
     console.log(thisData[1]);
     return thisData;
-  
 };
-
-var getData = function (subject) {  // "employer" vs "worksite" 
-
-    urlCntyStr = "https://raw.githubusercontent.com/aakims/american-dreams.temp/master/data/newDat/geojson/byCounty_" + subject + ".geojson",
-    urlStateStr = "https://raw.githubusercontent.com/aakims/american-dreams.temp/master/data/newDat/geojson/byState_" + subject + ".geojson";
-
-    actualDownload(urlCntyStr).done(filterData); 
-
-
+var fedData;
+           actualDownload.done(function(data) {
+                fedData = JSON.parse(data)
+                defineData(fedData); 
+            })
--- js/main.js
+++ js/main.js
@@ -1,115 +1,136 @@
-
-var thisData, alldata; 
-var dataset = "https://raw.githubusercontent.com/aakims/spice-rack/master/vdatafull_hq.json";
-var parsedData, mapData, yData; 
-var yExtent, yMin, yMax, yRange; 
-
-
-
-var prepData = function () {
-mapData = JSON.parse(JSON.stringify(thisData));
-console.log(mapData[0]);
-yData = 'all_salary'; 
-yExtent = d3.extent(mapData, function(county) { 
-//console.log(county[yData]); 
-	return county[yData]; });
-console.log(yExtent);
-
-yMin = yExtent[0];
-yMax = yExtent[1];
-//d3.min(mapData, function(county) { console.log(county[yData]); return county[yData]; });
-console.log(yMin); 
-
-//var yMax = d3.max(mapData, function(county) {console.log(county[yData]); return county[yData]; });
-console.log(yMax); 
-
-yRange = yMax - yMin;
-console.log(yRange);
-
-mapData = _.map(mapData, function (county) {
-	county = _.pick(county, 'cfips', yData); 
-	county[yData] = +county[yData];
-	return county; 
-}); 
-
-
-
-var svg = d3.select("svg");
-
-var path = d3.geoPath();
-
-var y = d3.scaleLinear()
-	.domain(yExtent)
-    .rangeRound([600, 860]);
-
-var quantile = d3.scaleQuantile()
-    .range(d3.range(9).map(function(i) { return "q" + i + "-9"; }));
-
-
-// var color = d3.scaleThreshold()
-//     .domain(yQuantile)
-//     .range(d3.schemeBlues[9]);
-
-var g = svg.append("g")
-    .attr("class", "key")
-    .attr("transform", "translate(0,40)");
-
-// g.selectAll("rect")
-//   .data(color.range().map(function(d) {
-//       d = color.invertExtent(d);
-//       if (d[0] == null) d[0] = yMin;
-//       if (d[1] == null) d[1] = yMax;
-//       return d;
-//     }))
-//   .enter().append("rect")
-//     .attr("height", 8)
-//     .attr("x", function(d) { return y(d[0]); })
-//     .attr("width", function(d) { return y(d[1]) - y(d[0]); })
-//     .attr("fill", function(d) { return color(d[0]); });
-
-//var h1b = d3.map(mapData, function (d) {return d["cfips"]}); 
-
-var yDataVal = d3.map(mapData, function (d) {return d["cfips"]*100	});
-mapData.forEach(function(d) { yDataVal[d.cfips] = + d[yData]});
-//h1b.set(d.cfips, +d.tot_worker); 
-
-d3.json("https://unpkg.com/us-atlas@1/us/10m.json", function(error, us) {
-  if (error) throw error;
-
-  svg.append("path")
-      .attr("stroke", "#aaa")
-      .attr("stroke-width", 0.5)
-      .attr("d", path(topojson.mesh(us, us.objects.counties, function(a, b) { return a !== b && (a.id / 1000 | 0) === (b.id / 1000 | 0); })));
-
-  svg.append("path")
-      .attr("stroke-width", 0.5)
-      .attr("d", path(topojson.mesh(us, us.objects.states, function(a, b) { return a !== b; })));
-
-  svg.append("path")
-      .attr("d", path(topojson.feature(us, us.objects.nation)));
-
-	quantile.domain(yExtent);
-
-  svg.append("g")
-  	.attr("class", "counties")
-  	.selectAll("path")
-  	.data(topojson.feature(us, us.objects.counties).features)
-  	.enter().append("path")
-  	.attr("class", function(d) {console.log(yDataVal[d.id]); console.log(quantile(yDataVal[d.id])); return quantile(yDataVal[d.id])})
-  	// .attr("fill", function(d) {console.log(yDataVal[d.id]); return color(yDataVal[d.id]);})
-  	.attr("d", path)
-  	.append("title");
-  	//.text(function(d) {return d.worker = .get(d.cfips)});
+<script>
+mapboxgl.accessToken = 'pk.eyJ1IjoiYWFraW1zIiwiYSI6ImNqZmQ1bm4yaDF4NnQzdW8xem54dmNzYXQifQ.VfaDRyNApyLYnCVL7PcpzA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/light-v9',
+    center: [31.4606, 20.7927],
+    zoom: 0.5
 });
-};
 
-$(document).ready(function() {
-  $.ajax(dataset).done(function(data) {
-  	alldata = data; 
-    parsedData = JSON.parse(data);
-    thisData = _.filter(parsedData, function(county) {
-    	return county["period"] === "2015Q4"; 
+// var months = [
+//     'January',
+//     'February',
+//     'March',
+//     'April',
+//     'May',
+//     'June',
+//     'July',
+//     'August',
+//     'September',
+//     'October',
+//     'November',
+//     'December'
+// ];
+
+//list of mappable elements: 'AVGSAL', 'APP', 'WORKER', 'PCTINSTATE'
+//col format: [quarterkey_resultkey_col]
+
+
+// function filterBy(month) {
+
+//     var filters = ['==', 'month', month];
+//     map.setFilter('earthquake-circles', filters);
+//     map.setFilter('earthquake-labels', filters);
+
+//     // Set the label to the month
+//     document.getElementById('month').textContent = months[month];
+// }
+
+function filterBy(quarter) {
+
+    var qprefix = empSt_quarterkey[quarter];
+
+
+
+    var filters = ['==', 'month', month];
+    map.setFilter('earthquake-circles', filters);
+    map.setFilter('earthquake-labels', filters);
+
+    // Set the label to the month
+    document.getElementById('month').textContent = months[month];
+}
+
+map.on('load', function() {
+
+    // Data courtesy of http://earthquake.usgs.gov/
+    // Query for significant earthquakes in 2015 URL request looked like this:
+    // http://earthquake.usgs.gov/fdsnws/event/1/query
+    //    ?format=geojson
+    //    &starttime=2015-01-01
+    //    &endtime=2015-12-31
+    //    &minmagnitude=6'
+    //
+    // Here we're using d3 to help us make the ajax request but you can use
+    // Any request method (library or otherwise) you wish.
+    d3.json('../data/newDat/geojson/byCounty_worksite.geojson', function(err, data) {
+        if (err) throw err;
+
+        // Create a month property value based on time
+        // used to filter against.
+        data.features = data.features.map(function(d) {
+            d.properties.month = new Date(d.properties.time).getMonth();
+            return d;
+        });
+
+        // map.addSource('earthquakes', {
+        //     'type': 'geojson',
+        //     'data': data
+        // });
+
+        map.addSource('statelev', {
+          'type': 'geojson',
+            'data': data
+        });
+
+        map.addLayer({
+            'id': 'earthquake-circles',
+            'type': 'circle',
+            'source': 'earthquakes',
+            'paint': {
+                'circle-color': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'mag'],
+                    6, '#FCA107',
+                    8, '#7F3121'
+                ],
+                'circle-opacity': 0.75,
+                'circle-radius': [
+                    'interpolate',
+                    ['linear'],
+                    ['get', 'mag'],
+                    6, 20,
+                    8, 40
+                ]
+            }
+        });
+
+        map.addLayer({
+            'id': 'earthquake-labels',
+            'type': 'symbol',
+            'source': 'earthquakes',
+            'layout': {
+                'text-field': ['concat', ['to-string', ['get', 'mag']], 'm'],
+                'text-font': ['Open Sans Bold', 'Arial Unicode MS Bold'],
+                'text-size': 12
+            },
+            'paint': {
+                'text-color': 'rgba(0,0,0,0.5)'
+            }
+        });
+
+        // Set filter to first month of the year
+        // 0 = January
+        filterBy(0);
+
+        document.getElementById('slider').addEventListener('input', function(e) {
+            var quarter = parseInt(e.target.value, 10);
+            filterBy(month);
+
+        // document.getElementById('slider').addEventListener('input', function(e) {
+        //     var month = parseInt(e.target.value, 10);
+        //     filterBy(month);
+        });
     });
-    prepData(); 
-}); 
 });
+</script>
